baaf4ffac99a5c7e9e4fb783ede8fdd03f5a51d4
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 03:43:52 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,2 +1,19 @@
 module Dance
+  def twirling
+    "I;m twirling"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you, it was a pleasure to dance for you all."
+  end
+  
+
 end
